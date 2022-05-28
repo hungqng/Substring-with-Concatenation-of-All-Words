e58019d909eb67399911b74e8a286a526c7b1548
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-27 20:31:16 -0700

    Add files via upload

--- Substring with Concatenation of All Words.py
+++ Substring with Concatenation of All Words.py
@@ -26,3 +26,44 @@ class Solution:
             if word_used == num_word:
                 result.append(i)
         return result
+
+        # Solution 2
+        if len(words) == 0:
+            return []
+        # initialize d, l, ans
+        l = len(words[0])
+        d = {}
+        for w in words:
+            if w in d:
+                d[w] += 1
+            else:
+                d[w] = 1
+        i = 0
+        ans = []
+
+        # sliding window(s)
+        for k in range(l):
+            left = k
+            subd = {}
+            count = 0
+            for j in range(k, len(s)-l+1, l):
+                tword = s[j:j+l]
+                # valid word
+                if tword in d:
+                    if tword in subd:
+                        subd[tword] += 1
+                    else:
+                        subd[tword] = 1
+                    count += 1
+                    while subd[tword] > d[tword]:
+                        subd[s[left:left+l]] -= 1
+                        left += l
+                        count -= 1
+                    if count == len(words):
+                        ans.append(left)
+                # not valid
+                else:
+                    left = j + l
+                    subd = {}
+                    count = 0
+        return ans
